f6f6504fcd01381330de8bb5fe4b80218586dcd6
Nelson6203 <Nelson2203@estudiantec.cr>
2024-03-31 01:47:39 -0600

    Add Header

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -10,12 +10,17 @@ import { LoginComponent } from './components/login/login.component';
 import { UserRegisterComponent } from './components/user-register/user-register.component';
 import { MenuPrincipalComponent } from './components/menu-principal/menu-principal.component';
 
+import { HeaderComponent } from './components/header/header.component';
+
+
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     UserRegisterComponent,
-    MenuPrincipalComponent
+    MenuPrincipalComponent,
+    HeaderComponent
+
   ],
   imports: [
     BrowserModule,
--- frontend/src/app/components/menu-principal/menu-principal.component.ts
+++ frontend/src/app/components/menu-principal/menu-principal.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu-principal',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class MenuPrincipalComponent {
 
+  constructor(private router: Router) { }
+
+  goToRoute(route: string) {
+    this.router.navigateByUrl(route);
+  }
+
 }
+
