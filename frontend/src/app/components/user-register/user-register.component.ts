cf83ba58186b62ed46883af7b5f55e9ffe0e28d9
Nelson6203 <Nelson2203@estudiantec.cr>
2024-03-30 18:25:25 -0600

    ADD RegisterUser

--- frontend/src/app/components/user-register/user-register.component.ts
+++ frontend/src/app/components/user-register/user-register.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { NgForm,FormGroup } from '@angular/forms';
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-user-register',
@@ -7,6 +11,43 @@ import { Component } from '@angular/core';
 })
 export class UserRegisterComponent {
 
-  constructor () {}
+  constructor(private httpClient: HttpClient, private router: Router) { }
+  registerForm!: FormGroup;
+
+
+  username: string = '';
+  email: string = '';
+  password: string = '';
+  isButtonDisabled: boolean = true;
+  errorMessage: string = '';
+
+  SERVER: string =  "";
+
+
+  onRegisterUser(form: NgForm) {
+    this.httpClient.post(`http://localhost:4000/api/registerUser`, form.value).subscribe(response => {
+      console.log(response);
+      this.router.navigateByUrl('/');
+    });
+  }
+
+
+  goToRoute(route: string) {
+    this.router.navigateByUrl(route);
+  }
+
+  validate(): void {
+    this.isButtonDisabled = this.username === '' || this.email === '' || this.password === '';
+    this.errorMessage = this.isButtonDisabled ? 'Los campos no pueden estar vacíos' : '';
+  }
+
+  showSuccessAlert() {
+    swal.fire({
+      icon: 'success',
+      title: 'Registro con éxito',
+      showConfirmButton: false,
+      timer: 1600
+    });
+  }
 
 }
