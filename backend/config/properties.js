a8c8ac63cb636e5ccae1f06a4c62610e17afc8bf
Nahomi Bolaños <120617689+nahomibv@users.noreply.github.com>
2024-04-29 02:12:29 -0600

    Change db name

--- backend/config/properties.js
+++ backend/config/properties.js
@@ -1,5 +1,5 @@
 module.exports = {
     PORT: process.env.PORT || 3000,
     IP:  '0.0.0.0',
-    DB: 'mongodb://127.0.0.1:27017/CompuDB'
+    DB: 'mongodb+srv://root:rootroot@mongobase.trc4ctz.mongodb.net/'
 }
