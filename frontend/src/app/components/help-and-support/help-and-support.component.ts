278ac46db5a632ff5a80ac449345b2c2b4d4b25c
Dsanabria02 <100260303+Dsanabria02@users.noreply.github.com>
2024-04-29 23:26:51 -0600

    added send mail

--- frontend/src/app/components/help-and-support/help-and-support.component.ts
+++ frontend/src/app/components/help-and-support/help-and-support.component.ts
@@ -1,15 +1,31 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { HttpClient } from '@angular/common/http';
+import swal from 'sweetalert2';
+import { environment } from 'src/environments/environment';
 
 @Component({
-  selector: 'app-help-and-support',
+  selector: 'app-user-profile',
   templateUrl: './help-and-support.component.html',
   styleUrls: ['./help-and-support.component.css']
 })
+
 export class HelpAndSupportComponent {
+
   tutorial1Visible = false;
   tutorial2Visible = false;
   tutorial3Visible = false;
   tutorial4Visible = false;
+  mensaje: string = '';
+  userData: any = {};
+  username: string = '';
+  isButtonDisabled: boolean = false;
+  errorMessage: string = '';
+  private URL = environment.BURL;
+
+  constructor(private httpClient: HttpClient, private authService: AuthService) {
+    this.getUserData();
+  }
 
   toggleTutorial1(): void {
     this.tutorial1Visible = !this.tutorial1Visible;
@@ -26,4 +42,42 @@ export class HelpAndSupportComponent {
   toggleTutorial4(): void {
     this.tutorial4Visible = !this.tutorial4Visible;
   }
+
+  getUserData() {
+    const storedUsername = this.authService.getUsername();
+    if (storedUsername !== null) {
+      this.httpClient.get(`${environment.BURL}/getUserData/` + storedUsername).subscribe(
+        (data: any) => {
+          this.userData = data;
+          console.log('Datos del usuario:', this.userData);
+        },
+        (error) => {
+          console.log('Error al obtener datos del usuario:', error);
+        }
+      );
+    } else {
+      console.error('Error: No se encontró un nombre de usuario almacenado en la sesión.');
+    }
+  }
+
+  enviarMensaje(): void {
+    if (this.mensaje.trim() !== '') {
+      const mensajeData = {
+        usuario: this.userData.username,
+        id: this.userData._id,
+        nombre: this.userData.username,
+        correo: this.userData.email,
+        consulta: this.mensaje
+      };
+      this.httpClient.post(`${this.URL}/send-email`, mensajeData)
+        .subscribe(response => {
+          console.log('Respuesta del backend:', response); 
+          
+        }, error => {
+          console.error('Error al enviar el mensaje:', error); 
+          
+        });
+    }
+  }
+
 }
