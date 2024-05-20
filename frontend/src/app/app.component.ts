34fedfe9be646dc4e45d49e6ce585c6a07711750
Nelson6203 <Nelson2203@estudiantec.cr>
2024-05-20 15:10:54 -0600

    ADD Google Analytics

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+
+declare var gtag: Function;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'frontend';
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        gtag('config', 'G-JBNCMVXPEJ', {
+          'page_path': event.urlAfterRedirects
+        });
+      }
+    });
+  }
 }
